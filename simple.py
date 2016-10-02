f74e6972baebf14a3a970f619380160dd066b6ba
Jody-Lu <b00901192@ntu.edu.tw>
2016-10-01 20:23:54 -0500

    complete the simple.py

--- simple.py
+++ simple.py
@@ -23,3 +23,35 @@ system.membus = CoherentXBar()
 system.cpu.icache_port = system.membus.slave
 system.cpu.dcache_port = system.membus.slave
 
+# create the interrupt controller for the CPU and connect to the membus
+system.cpu.createInterruptController()
+system.cpu.interrupts.pio = system.membus.master
+system.cpu.interrupts.int_master = system.membus.slave
+system.cpu.interrupts.int_slave = system.membus.master
+
+# Create a DDR3 memory controller and connect it to the membus
+system.mem_ctrl = DDR3_1600_x64()
+system.mem_ctrl.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.master
+
+# Connect the system up to the membus
+system.system_port = system.membus.slave
+
+# Create a process for a simple "Hello World" application
+process = LiveProcess()
+# Set the command
+# cmd is a list which begins with the executable (like argv)
+process.cmd = ['tests/test-progs/hello/bin/x86/linux/hello']
+# Set the cpu to use the process as its workload and create thread contexts
+system.cpu.workload = process
+system.cpu.createThreads()
+
+# set up the root SimObject and start the simulation
+root = Root(full_system = False, system = system)
+# instantiate all of the objects we've created above
+m5.instantiate()
+
+print "Beginning simulation!"
+exit_event = m5.simulate()
+print 'Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause())
+
